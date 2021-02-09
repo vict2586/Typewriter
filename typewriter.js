15662d33e6a7805abbecaab631d2b791abcfacc3
Victoria <vigi2586@stud.kea.dk>
2021-02-09 15:17:52 +0100

    Code started

---
+++ typewriter.js
@@ -0,0 +1,18 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", start);
+
+let text;
+
+let showtext;
+
+let sentenceOne = document.querySelector("#typewriterOne");
+let sentenceToo = document.querySelector("#typewriterToo");
+let sentenceThree = document.querySelector("#typewriterThree");
+let sentenceFour = document.querySelector("#typewriterFour");
+let sentenceFive = document.querySelector("#typewriterFive");
+let sentenceSix = document.querySelector("#typewriterSix");
+
+let i = 1;
+
+
